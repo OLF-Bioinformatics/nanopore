7fc0d448f81039bfb48790f9b980aaf49005c09d
Marc-Olivier Duceppe <marc-olivier.duceppe@inspection.gc.ca>
2019-01-17 14:36:26 -0500

    code optimised to use one run call
    All the code put inside the call.
    Only use one "run"call if input is a folder of a file.

--- nanopore_pass_faill_splitter.py
+++ nanopore_pass_faill_splitter.py
@@ -1,15 +1,15 @@
 #!/usr/local/env python3.6
 
-__version__ = '0.2'
+__version__ = '0.3'
 __author__ = ['duceppemo', 'chmaraj']
 
-
 import gzip
-from math import log
+# from math import log
 import os
 import pathlib
-from multiprocessing import Process
 from multiprocessing import Pool
+import functions
+
 
 class Splitter(object):
 
@@ -19,85 +19,76 @@ class Splitter(object):
         self.threshold = args.qscore
         self.threads = args.threads
 
-        if os.path.isdir(self.input_fastq):
-            self.pass_dir = ''.join([self.output_folder, 'pass'])
-            self.fail_dir = ''.join([self.output_folder, 'fail'])
-            pathlib.Path(self.pass_dir).mkdir(parents=True, exist_ok=True)
-            pathlib.Path(self.fail_dir).mkdir(parents=True, exist_ok=True)
-            self.run_folder()
-        else:
-            sample_name = os.path.basename(self.input_fastq).split('.')[0]
-            pass_file = ''.join([self.output_folder, '/', sample_name, '_pass.fastq.gz'])
-            fail_file = ''.join([self.output_folder, '/', sample_name, '_fail.fastq.gz'])
-            self.pass_file_handle = gzip.open(pass_file, 'w')
-            self.fail_file_handle = gzip.open(fail_file, 'w')
+        # Set the output folder paths for pass and fail reads
+        self.pass_dir = '/'.join([self.output_folder, 'pass'])
+        self.fail_dir = '/'.join([self.output_folder, 'fail'])
 
-            self.run()
+        # Create the folders if they don't exist
+        pathlib.Path(self.pass_dir).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(self.fail_dir).mkdir(parents=True, exist_ok=True)
+
+        # Run
+        self.run()
 
     def run(self):
-        self.parse_fastq(self.input_fastq)
-        self.close_handle(self.pass_file_handle)
-        self.close_handle(self.fail_file_handle)
-        
-    def run_folder(self):
-        file_list = []
-        for item in os.listdir(self.input_fastq):
-            sample_name = item.split(".")[0]
-            pass_file = ''.join([self.pass_dir, '/', sample_name, '_pass.fastq.gz'])
-            fail_file = ''.join([self.fail_dir, '/', sample_name, '_fail.fastq.gz']
-            sample_file = os.path.dirname(self.input_fastq) + '/' + item
-            passed_items = [sample_file, pass_file, fail_file]
-            file_list.append(passed_items)
+        fastq_list = []
+
+        if os.path.isfile(self.input_fastq):
+            self.is_fastq(self.input_fastq)  # check if right file extension
+            fastq_list.append(self.input_fastq)  # just put that file in the list
+        elif os.path.isdir(self.input_fastq):
+            for root, directories, filenames in os.walk(self.input_fastq):  # look recursively
+                for filename in filenames:
+                    absolute_path = os.path.join(root, filename)
+                    if os.path.isfile(absolute_path) and filename.endswith(('.fastq', '.fastq.gz', '.fq', '.fq.gz')):
+                        fastq_list.append(absolute_path)
+        else:
+            raise Exception("Something is wrong with your input.")
+
+        # check if input_fastq_list is not empty
+        if not fastq_list:
+            raise Exception("Input fastq file not found!" if os.path.isfile(self.input_fastq)
+                            else "No fastq files found in input folder")
+
+        io_tuple_list = []
+        for fastq in fastq_list:
+            sample_name = os.path.basename(fastq).split(".")[0]  # everything before the fist dot (".")
+
+            # Create the path for the output file
+            output_pass_file = ''.join([self.pass_dir, '/', sample_name, '_pass.fastq.gz'])
+            output_fail_file = ''.join([self.fail_dir, '/', sample_name, '_fail.fastq.gz'])
+
+            io_tuple_list.append((fastq, output_pass_file, output_fail_file))
+
+        # Run samples in parallel
+        # Initiate the pool
         pool = Pool(self.threads)
-        output = pool.map_async(parse_fastq_parallel, file_list)
+
+        # Parse the fastq files in parallel asynchronously
+        pool.map_async(self.parse_fastq_parallel, io_tuple_list)
+
+        # Close the pool
         pool.close()
         pool.join()
 
-    def parse_fastq(self, fastq):
-
-        with gzip.open(fastq, 'rb', 1024 * 1024) if fastq.endswith('gz') else open(fastq, 'rb', 1024 * 1024) as f:
-            lines = []
-            for line in f:
-                if not line:  # end of file?
-                    continue
-                line = line.rstrip()
-
-                # if lines and b'runid' in line:  # new entry -> specific to nanopore
-                if len(lines) == 4:
-                    self.process_fastq_entry(lines)
-                    lines = []
-                lines.append(line)
-            self.process_fastq_entry(lines)  # For the last entry
-
-    def process_fastq_entry(self, my_list):
-        header, seq, extra, qual = my_list  # get each component of list in a variable
-
-        # Average phred score
-        phred_list = [letter - 33 for letter in qual]
-        average_phred = -10 * log(sum([10 ** (q / -10) for q in phred_list]) / len(phred_list), 10)
-
-        flag = 'pass'
-        if average_phred < 7:
-            flag = 'fail'
-
-        self.write_n_compress(flag, header, seq, extra, qual)
-
-    def write_n_compress(self, flag, header, seq, extra, qual):
-        flag_dict = {'pass': self.pass_file_handle,
-                     'fail': self.fail_file_handle
-                     }
-
-        flag_dict[flag].write(header + b'\n' + seq + b'\n' + extra + b'\n' + qual + b'\n')
+    def is_fastq(self, fastq):
+        if not 'fastq' or 'fq' in fastq:
+            raise Exception('Only files with the following extensions are accepted:\
+            ".fastq", ".fq", ".fastq.gz" or ".fq.gz" ')
 
     def close_handle(self, handle):
         handle.close()
-                                
-def parse_fastq_parallel(file_list):
-        pass_file_handle = gzip.open(file_list[1], 'w')
-        fail_file_handle = gzip.open(file_list[2], 'w')
-        output_list = [pass_file_handle, fail_file_handle]
-        with gzip.open(file_list[0], 'rb', 1024 * 1024) if \
-            file_list[0].endswith('gz') else open(file_list[0], 'rb', 1024 * 1024) as f:
+
+    def parse_fastq_parallel(self, io_tuple):
+        # Create output file handles
+        in_fastq, pass_fastq, fail_fastq = io_tuple
+
+        pass_file_handle = gzip.open(pass_fastq, 'w')
+        fail_file_handle = gzip.open(fail_fastq, 'w')
+
+        handles_tuple = (pass_file_handle, fail_file_handle)
+        with gzip.open(in_fastq, 'rb', 1024 * 1024) if \
+                in_fastq.endswith('gz') else open(in_fastq, 'rb', 1024 * 1024) as f:
             lines = []
             for line in f:
                 if not line:  # end of file?
@@ -106,40 +97,42 @@ def parse_fastq_parallel(file_list):
 
                 # if lines and b'runid' in line:  # new entry -> specific to nanopore
                 if len(lines) == 4:
-                    process_fastq_entry_parallel(lines, output_list)
+                    self.process_fastq_entry(lines, handles_tuple)
                     lines = []
                 lines.append(line)
-            process_fastq_entry_parallel(lines, output_list)
+            self.process_fastq_entry(lines, handles_tuple)
 
-def process_fastq_entry_parallel(my_list, output_list):
+        self.close_handle(pass_file_handle)
+        self.close_handle(fail_file_handle)
+
+    def process_fastq_entry(self, my_list, output_list):
         header, seq, extra, qual = my_list  # get each component of list in a variable
 
         # Average phred score
         phred_list = [letter - 33 for letter in qual]
-        average_phred = -10 * log(sum([10 ** (q / -10) for q in phred_list]) / len(phred_list), 10)
+        # average_phred = -10 * log(sum([10 ** (q / -10) for q in phred_list]) / len(phred_list), 10)
+        average_phred = functions.compute_average_quality(phred_list, len(seq))  # cython
 
         flag = 'pass'
-        if average_phred < 7:
+        if average_phred < self.threshold:
             flag = 'fail'
 
-        write_n_compress_parallel(flag, header, seq, extra, qual, output_list)
+        self.write_n_compress(flag, header, seq, extra, qual, output_list)
 
-def write_n_compress_parallel(flag, header, seq, extra, qual, output_list):
+    def write_n_compress(self, flag, header, seq, extra, qual, output_list):
         flag_dict = {'pass': output_list[0],
-                     'fail': output_list[1]
-                     }
+                     'fail': output_list[1]}
 
         flag_dict[flag].write(header + b'\n' + seq + b'\n' + extra + b'\n' + qual + b'\n')
 
 
 if __name__ == '__main__':
-
     from argparse import ArgumentParser
 
     parser = ArgumentParser(description='Split a fastq file into pass and fail reads based on average phred score ')
-    parser.add_argument('-i', '--input', metavar='sample.fastq.gz',
-                        required=False,
-                        help='Input folder with fastq file(s),gzipped or not')
+    parser.add_argument('-i', '--input', metavar='sample.fastq.gz|/my/folder',
+                        required=True,
+                        help='An indidual fastq file or a folder with fastq file(s), gzipped or not')
     parser.add_argument('-o', '--output', metavar='/output/folder/',
                         required=True,
                         help='Output folder'
@@ -148,7 +141,8 @@ if __name__ == '__main__':
                         required=False,
                         help='Qscore (average phred score) used to split pass reads from fail reads'
                              'Default is 7')
-    parser.add_argument('-t', '--threads', metavar='1', type=int, default=1, required=False, 
+    parser.add_argument('-t', '--threads', metavar='1', type=int, default=1,
+                        required=False,
                         help='Threads used in case of multiprocessing files in a folder'
                              'Default is 1')
 
