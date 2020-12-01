aa1d54a7ae2ecad64416447b4294cae9f6de73e4
Marc-Olivier Duceppe <17859096+duceppemo@users.noreply.github.com>
2020-11-30 22:17:07 -0500

    Update format_fasta.py
    Ditched biopython to parse the fasta and wrote my own parser.

--- format_fasta.py
+++ format_fasta.py
@@ -1,5 +1,4 @@
 import sys
-from Bio.SeqIO.FastaIO import SimpleFastaParser
 import textwrap
 
 input_fasta = sys.argv[1]
@@ -8,10 +7,22 @@ output_fasta = sys.argv[2]
 fasta_dict = dict()
 
 with open(input_fasta, 'r') as in_handle:
-    for title, seq in SimpleFastaParser(in_handle):
-        fasta_dict[title] = seq
+    header = ''
+    seq = list()
+    for line in in_handle:
+        line = line.rstrip()
+        if not line:
+            continue
+        if line.startswith('>'):
+            if seq:
+                fasta_dict[header] = ''.join(seq)  # Store in dictionary
+                seq = list()  # empty seq
+            header = line
+        else:
+            seq.append(line)
+        # last entry
+        fasta_dict[header] = ''.join(seq)
 
 with open(output_fasta, 'w') as out_handle:
     for title, seq in fasta_dict.items():
         out_handle.write('>{}\n{}\n'.format(title, '\n'.join(textwrap.wrap(seq, 80, break_long_words=True))))
-        
