ea60f8d52e21bc0153a83b0e43c6550e1227dda8
Marc-Olivier Duceppe <17859096+duceppemo@users.noreply.github.com>
2022-03-15 15:47:12 -0400

    Update format_fasta.py

--- format_fasta.py
+++ format_fasta.py
@@ -25,4 +25,4 @@ with open(input_fasta, 'r') as in_handle:
 
 with open(output_fasta, 'w') as out_handle:
     for title, seq in fasta_dict.items():
-        out_handle.write('>{}\n{}\n'.format(title, '\n'.join(textwrap.wrap(seq, 80, break_long_words=True))))
+        out_handle.write('{}\n{}\n'.format(title, '\n'.join(textwrap.wrap(seq, 80, break_long_words=True))))
